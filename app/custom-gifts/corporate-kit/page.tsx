4745871b22a0144f72d4e25881970ea7d9740a3a
sinu00 <cssinu7@gmail.com>
2025-06-24 12:19:05 +0530

    custom gift image name fixed

--- app/custom-gifts/corporate-kit/page.tsx
+++ app/custom-gifts/corporate-kit/page.tsx
@@ -7,7 +7,7 @@ export default function CorporateKitPage() {
       <section className="py-16 lg:py-24 bg-white">
         <div className="max-w-3xl mx-auto px-4 text-center">
           <Image
-            src="/custom-gifts/Corporate-kit.jpg"
+            src="/custom-gifts/Corporate-Kit.jpg"
             alt="Corporate Kit"
             width={288}
             height={176}
